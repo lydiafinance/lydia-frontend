02566bd38212800605753e825c12d8535a586071
lydiacoder <hello@lydia.finance>
2021-05-13 11:11:34 +0300

    enhanced ui && fixed calculation modal non issue

--- src/views/Farms/components/FarmTable/Liquidity.tsx
+++ src/views/Farms/components/FarmTable/Liquidity.tsx
@@ -1,53 +1,55 @@
 import React from 'react'
 import styled from 'styled-components'
-import { HelpIcon, Text } from '@lydiafinance/uikit'
+import { HelpIcon, Text, Skeleton, useTooltip } from '@lydiafinance/uikit'
 import { useTranslation } from 'contexts/Localization'
+import BigNumber from 'bignumber.js'
 
-import Tooltip from '../Tooltip/Tooltip'
+const ReferenceElement = styled.div`
+  display: inline-block;
+`
 
 export interface LiquidityProps {
-  liquidity: number
+  liquidity: BigNumber
 }
 
 const LiquidityWrapper = styled.div`
   min-width: 110px;
   font-weight: 600;
   text-align: right;
+  margin-right: 14px;
 
-  ${({ theme }) => theme.mediaQueries.sm} {
+  ${({ theme }) => theme.mediaQueries.lg} {
     text-align: left;
+    margin-right: 0;
   }
 `
 
 const Container = styled.div`
   display: flex;
   align-items: center;
-
-  svg {
-    margin-left: 14px;
-  }
-
-  ${({ theme }) => theme.mediaQueries.sm} {
-    svg {
-      margin-left: 0;
-    }
-  }
 `
 
 const Liquidity: React.FunctionComponent<LiquidityProps> = ({ liquidity }) => {
-  const displayLiquidity = liquidity
-    ? `$${Number(liquidity).toLocaleString(undefined, { maximumFractionDigits: 0 })}`
-    : '-'
+  const displayLiquidity = liquidity ? (
+    `$${Number(liquidity).toLocaleString(undefined, { maximumFractionDigits: 0 })}`
+  ) : (
+    <Skeleton width={60} />
+  )
   const { t } = useTranslation()
+  const { targetRef, tooltip, tooltipVisible } = useTooltip(
+    t('The total value of the funds in this farm’s liquidity pool'),
+    { placement: 'top-end', tooltipOffset: [20, 10] },
+  )
 
   return (
     <Container>
       <LiquidityWrapper>
         <Text>{displayLiquidity}</Text>
       </LiquidityWrapper>
-      <Tooltip content={t('The total value of the funds in this farm’s liquidity pool')}>
+      <ReferenceElement ref={targetRef}>
         <HelpIcon color="textSubtle" />
-      </Tooltip>
+      </ReferenceElement>
+      {tooltipVisible && tooltip}
     </Container>
   )
 }

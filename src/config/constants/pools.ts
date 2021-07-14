1abd72bbf80ee369711f46c97db02194e6aca3fc
manes <manes@lydia.finance>
2021-07-14 17:29:34 +0300

    adds wavax pool

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -65,6 +65,20 @@ const pools: PoolConfig[] = [
     sortOrder: 1,
     isFinished: true,
   },
+  {
+    sousId: 1004,
+    stakingToken: tokens.lyd,
+    earningToken: tokens.wavax,
+    contractAddress: {
+      [ChainId.FUJI]: '0x89dAA415A13B6146787B4a1EF22dF4Ef7589b0bf',
+      [ChainId.AVALANCHE]: '0x89dAA415A13B6146787B4a1EF22dF4Ef7589b0bf',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '0.00027',
+    sortOrder: 1,
+    isFinished: false,
+  },
 ]
 
 export default pools

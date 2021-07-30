e60a6a306ed92de7521fdd48b62d5a9395c687c4
manes <manes@lydia.finance>
2021-07-30 11:07:34 +0300

    checkpoint

--- src/views/Home/components/FarmStakingAddCard.tsx
+++ src/views/Home/components/FarmStakingAddCard.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useCallback, useEffect} from 'react'
-
+import BigNumber from 'bignumber.js'
 import {useWeb3React} from "@web3-react/core";
 
 import useLastUpdated from "../../../hooks/useLastUpdate";
@@ -7,6 +7,10 @@ import useGetVaultUserInfo from "../../../hooks/lydVault/useGetVaultUserInfo";
 import {convertSharesToLyd} from "../../Pools/helpers";
 import {getFullDisplayBalance} from "../../../utils/formatBalance";
 import useGetVaultSharesInfo from "../../../hooks/lydVault/useGetVaultSharesInfo";
+import {useMaximusPools} from "../../../state/hooks";
+import useGetMaximusUserInfo from "../../../hooks/maximus/useGetMaximusUserInfo";
+import useGetMaximusFees from "../../../hooks/maximus/useGetMaximusFees";
+import useGetMaximusSharesInfo from "../../../hooks/maximus/useGetMaximusSharesInfo";
 
 
 const FarmedStakingAddCard = () => {
@@ -22,7 +26,35 @@ const FarmedStakingAddCard = () => {
     const lydProfit = currentSharesAsLyd.lydAsBigNumber.minus(userVaultInfo.lydAtLastUserAction)
     const lydToDisplay = lydProfit.gte(0) ? getFullDisplayBalance(lydProfit, 18, 5) : '0'
 
+    // -----
+    const pools = useMaximusPools(account);
 
+
+    let maximus = new BigNumber("0");
+    console.log(maximus)
+
+
+    pools.forEach(p => {
+        maximus = maximus.plus(p.userData.pendingReward);
+    })
+
+    console.log(maximus.toString())
+
+
+    /*
+
+    const { pricePerFullShare2 } = useGetMaximusSharesInfo()
+
+    const { lpSymbol, totalStaked, userData } = pool
+    const { depositAt, stakedBalance } = userData || {}
+    //   Estimate & manual for now. 288 = once every 5 mins. We can change once we have a better sense of this
+    const timesCompoundedDaily = 12
+    const accountHasSharesStaked = stakedBalance && stakedBalance.gt(0)
+    // const stakingTokenPrice = useGetApiPrice(stakingToken?.symbol?.toLowerCase())
+    const stakingTokenPrice = 1
+    const isLoading = !pool.userData || !userInfo.shares
+    const performanceFeeAsDecimal = vaultFees.performanceFee && parseInt(vaultFees.performanceFee, 10) / 100
+    */
     return (
         <span>{lydToDisplay} LYD</span>
     );

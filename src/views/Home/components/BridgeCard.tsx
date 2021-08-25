37926d0c4dc471cc2a0c6bdbd1a4feb968762dd8
lydiacoder <hello@lydia.finance>
2021-08-25 12:35:16 +0300

    added mobile

--- src/views/Home/components/BridgeCard.tsx
+++ src/views/Home/components/BridgeCard.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import { Card, Heading } from '@lydiafinance/uikit'
 
 import { useTranslation } from '../../../contexts/Localization'
+import useDeviceSize from '../../../hooks/useWindowSize'
 
 const StyledBridgeCard = styled(Card)`
   color: ${({ theme }) => theme.colors.avalanche};
@@ -24,23 +25,27 @@ const CardBody = styled.a`
   align-items: center;
 `
 
-const BridgeImage = styled(Heading)`
+const BridgeImage = styled(Heading)<{ isMobile: boolean }>`
   background: #ffffffa6;
   border-radius: 10px;
   padding: 5px;
-  font-size: 40px;
+  font-size: ${({ isMobile }) => `${isMobile ? '30px' : '40px'}`};
   color: #000px;
 `
 
+// font-size: 30px;
+
 const BridgeCard = () => {
   const { t } = useTranslation()
+  const deviceSize = useDeviceSize()
+  const { isMobile } = deviceSize
 
   return (
     <StyledBridgeCard>
       <CardBody href="https://gate.lydia.finance">
         {/* <BridgeImage src="/images/gate.svg" /> */}
 
-        <BridgeImage color="#000" scale="xl">
+        <BridgeImage isMobile={isMobile} color="#000" scale="xl">
           {t('LYD Gate | Buy LYD on BSC')}
         </BridgeImage>
       </CardBody>

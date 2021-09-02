b6e6d21c9d1b82b5de2d8cd6eefd35371f3dd0f4
manes-codes <manes@lydia.finance>
2021-09-02 14:18:00 +0300

    auto switch staked-only

--- src/views/Maximus/components/PoolTabButtons.tsx
+++ src/views/Maximus/components/PoolTabButtons.tsx
@@ -15,6 +15,7 @@ import { Link, useLocation, useRouteMatch } from 'react-router-dom'
 
 const ButtonText = styled(Text)`
   display: none;
+
   ${({ theme }) => theme.mediaQueries.lg} {
     display: block;
   }
@@ -47,9 +48,15 @@ const PoolTabButtons = ({ stakedOnly, setStakedOnly, hasStakeInFinishedFarms }) 
   }
 
   return (
-    <Flex alignItems="center" justifyContent="center" mb="32px" flexDirection={['column', null, 'row', null]}>
+    <Flex alignItems='center' justifyContent='center' mb='32px' flexDirection={['column', null, 'row', null]}>
       <Flex alignItems='center' flexDirection={['column', null, 'row', null]} mb={[10, null, 0]}>
-        <ButtonMenu activeIndex={activeIndex} scale='sm' variant='subtle'>
+        <ButtonMenu activeIndex={activeIndex} scale='sm' variant='subtle' onItemClick={(index) => {
+          if (index === 0) {
+            setStakedOnly(false)
+          } else if (index === 1) {
+            setStakedOnly(true)
+          }
+        }}>
           <ButtonMenuItem as={Link} to={`${url}`}>
             {t('Live')}
           </ButtonMenuItem>
@@ -60,19 +67,19 @@ const PoolTabButtons = ({ stakedOnly, setStakedOnly, hasStakeInFinishedFarms }) 
           </NotificationDot>
         </ButtonMenu>
       </Flex>
-      <Flex alignItems="center" flexDirection={['column', null, 'row', null]}  mb={[10, null, 0]}>
-        <Flex mt={['4px', null, 0, null]} ml={[0, null, '24px', null]} justifyContent="center" alignItems="center">
-          <Toggle scale="sm" checked={stakedOnly} onChange={() => setStakedOnly((prev) => !prev)} />
-          <Text ml="8px">{t('Staked only')}</Text>
+      <Flex alignItems='center' flexDirection={['column', null, 'row', null]} mb={[10, null, 0]}>
+        <Flex mt={['4px', null, 0, null]} ml={[0, null, '24px', null]} justifyContent='center' alignItems='center'>
+          <Toggle scale='sm' checked={stakedOnly} onChange={() => setStakedOnly((prev) => !prev)} />
+          <Text ml='8px'>{t('Staked only')}</Text>
         </Flex>
       </Flex>
-      <Flex ml="24px" alignItems="center" justifyContent="flex-end">
-        <StyledLink external href="https://docs.lydia.finance/guides/maximus/maximus-2">
-          <Button px={['14px', null, null, null, '20px']} variant="subtle">
-            <ButtonText color="backgroundAlt" bold fontSize="16px">
+      <Flex ml='24px' alignItems='center' justifyContent='flex-end'>
+        <StyledLink external href='https://docs.lydia.finance/guides/maximus/maximus-2'>
+          <Button px={['14px', null, null, null, '20px']} variant='subtle'>
+            <ButtonText color='backgroundAlt' bold fontSize='16px'>
               {t('Help')}
             </ButtonText>
-            <HelpIcon color="backgroundAlt" ml={[null, null, null, 0, '6px']} />
+            <HelpIcon color='backgroundAlt' ml={[null, null, null, 0, '6px']} />
           </Button>
         </StyledLink>
       </Flex>
